540049ed9a66c49b441f350b024e614a7d36631f
Alexander Barber <alex@dangerzonegames.com>
2018-10-19 23:44:09 -0600

    Created the plugin entry point.

---
+++ lenspress.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Plugin Name: LensPress
+ * Plugin URI: https://github.com/aebarber/LensPress
+ * Description: A WordPress Gutenberg editor plugin for conditional content
+ * Version: 0.1.0
+ * Author: Danger Interactive LLC
+ *
+ * @package LensPress
+ */
+ 
+const LENSPRESS_VERSION = 0.1.0;
+
+// TODO: make sure Gutenberg is installed, activated, and up-to-date.
+
+// TODO: register blocks for gutenberg.
+
+// TODO: register shortcodes that the gutenberg blocks will produce.
+
+declare(strict_types=1);
+
